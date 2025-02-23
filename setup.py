f8ead1bc4a7b9d0de9cc8bfaac904461459ca377
YaoxinHuang <huang_yaoxin@163.com>
2025-02-23 12:41:35 +0800

    version 1.0

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='yaoxin_tools',
-    version='0.1',
+    name='yxtools',
+    version='1.0',
     packages=find_packages(),
     install_requires=[
         # 列出你的工具包依赖的其他包
-        'numpy==1.24.0',
+        'numpy',
         'pillow',
         'SimpleITK'
     ],
